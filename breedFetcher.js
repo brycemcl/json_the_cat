b2e37e507d51a2a3f063d21c70ce23d33cb993d2
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-15 00:24:10 +0000

    factored out frontend logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,27 @@
 const { promisify } = require('util');
 const request = promisify(require("request"));
-const arg = process.argv.slice(2)[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`)
-  .then(({ body }) => {
-    return JSON.parse(body);
-  })
-  .catch(
-    () => {
-      console.log("Error: There has been a network error.");
-      return false;
-    }
-  )
-  .then((body) => {
-    if (body === false) {
-      console.log("Error: Breed Not found.");
-    } else {
-      console.log(body[0].description);
-    }
-  })
-  .catch(
-    () => { }
-  );
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`)
+    .then(({ body }) => {
+      return JSON.parse(body);
+    })
+    .catch(
+      () => {
+        callback("Error: There has been a network error.", null);
+        return false;
+      }
+    )
+    .then((body) => {
+      if (body === false) {
+        callback("Error: Breed Not found.", null);
+      } else {
+        callback(null, body[0].description);
+      }
+    })
+    .catch(
+      () => { }
+    );
+};
+
+module.exports = { fetchBreedDescription };
